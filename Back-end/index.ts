6bff05d45742124ce8680322b9e2454a11816e35
Duy Huynh <duy.huynh@citynow.vn>
2020-12-06 16:55:31 +0700

    pages login and signup

--- Back-end/index.ts
+++ Back-end/index.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express from "express";
 // rest of the code remains same
 const app = express();
-const PORT = 8000;
-app.get('/', (req, res) => res.send('Typescript'));
+const PORT = 8000 || process.env.PORT;
+app.get("/", (req, res) => res.send("Typescript"));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
---
+++ Front-end/src/types/FormData/index.ts
@@ -0,0 +1,10 @@
+export type FormData = {
+  email: string;
+  password: string;
+};
+
+export type FormDataSignUp = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
